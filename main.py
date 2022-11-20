3010d247cfe7a2c6b7be2d4b311be52a4e026914
realhardik18 <realhardik18@gmail.com>
2022-11-20 10:35:44 +0530

    messing around with sample data

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from pymongo.mongo_client import MongoClient
 from creds import DATABSE_URL
 
 client = MongoClient(DATABSE_URL)
-print(client.test)
+database=client.get_database('sample_airbnb')
+collection=database.get_collection('listingsAndReviews')
+#print(collection.insert_one({'hardik':'Cool'}))
+print(collection.find_one({"_id" :"10006546"}))
